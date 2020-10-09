f5cbf9def711d3d89c54a72d3b3393834f3c542c
Виталий Качурин <citrix@mail.ru>
2020-10-09 18:14:18 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,21 +2,22 @@
 
 use IlluminateHttpRequest;
 
-$app = require __DIR__.'/../bootstrap/app.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
 
-class Dev extends IlluminateDatabaseEloquentModel {
+class Dev extends IlluminateDatabaseEloquentModel
+{
     protected $table = 'devs';
 }
 
-$app->get('dev', function() {
+$app->get('dev', function () {
     return response()->json(Dev::all());
 });
 
-$app->get('dev/{id}', function($id) {
+$app->get('dev/{id}', function ($id) {
     return response()->json(Dev::find($id));
 });
 
-$app->post('dev', function(Request $request) {
+$app->post('dev', function (Request $request) {
     $dev = new Dev();
     $dev->name = $request->input('name');
     $dev->focus = $request->input('focus');
@@ -26,12 +27,12 @@ $app->post('dev', function(Request $request) {
     return response()->json($dev, 201);
 });
 
-$app->delete('dev/{id}', function($id) {
+$app->delete('dev/{id}', function ($id) {
     Dev::find($id)->delete();
     return response('', 200);
 });
 
-$app->patch('dev/{id}', function(Request $request, $id) {
+$app->patch('dev/{id}', function (Request $request, $id) {
     $dev = Dev::find($id);
     $dev->name = $request->input('name', $dev->name);
     $dev->focus = $request->input('focus', $dev->focus);
